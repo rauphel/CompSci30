e8d87cf2e23fa5c59a48c43b15991153a65ee044
rauphel <rauphelcyreuskenept@gmail.com>
2025-10-10 19:48:32 -0600

    added block position restrictions

--- array-object-assignment/sketch.js
+++ array-object-assignment/sketch.js
@@ -7,21 +7,24 @@
 
 let blockSize = 50;
 let spawnHeight;
-let screenHeight;
+let screen;
 let screenWidth = 500;
+let theBlocks = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   spawnHeight = height/2;
-  screenHeight = height;
+  // screenHeight = height;
   aBlock = spawnBlocks(width/2);
-
+  screen = createScreen();
 }
 
 function draw() {
   background(220);
-  screen();
-  rect(aBlock.x1, aBlock.y1, aBlock.blockWidth, aBlock.blockHeight);
+  // screen();
+  rect(screen.x, screen.y, screen.w, screen.h)
+  fill('white');
+  rect(aBlock.x1, aBlock.y1, aBlock.w, aBlock.h);
   gravity();
 }
 
@@ -31,8 +34,8 @@ function spawnBlocks(x) {
   let blocks = {
     x1: x,
     y1: spawnHeight,
-    blockWidth: blockSize * _width,
-    blockHeight: blockSize,
+    w: blockSize * _width,
+    h: blockSize,
     grav: 5,
   };
   return blocks;
@@ -46,10 +49,15 @@ function gravity() {
 }
 
 function dragBlock() {
-  if (mouseX >= aBlock.x1 && mouseX <= aBlock.x1 + aBlock.blockWidth 
-      && mouseY >= aBlock.y1 && mouseY <= aBlock.y1 + aBlock.blockHeight) {
-    console.log(true);
-    aBlock.x1 = mouseX - aBlock.blockWidth/2;
+  if (mouseX >= aBlock.x1 && mouseX <= aBlock.x1 + aBlock.w 
+      && mouseY >= aBlock.y1 && mouseY <= aBlock.y1 + aBlock.h) {
+    aBlock.x1 = mouseX - aBlock.w/2;
+    if (aBlock.x1 <= screen.x) {
+      aBlock.x1 = screen.x;
+    }
+    else if (aBlock.x1 + aBlock.w >= screen.x + screen.w) {
+      aBlock.x1 = screen.x + screen.w - aBlock.w;
+    }
   }
 }
 
@@ -59,7 +67,31 @@ function mouseDragged() {
   }
 }
 
-function screen() {
-  fill('gray');
-  rect(width/2 - screenWidth/2, 0, screenWidth, screenHeight);
+function mouseReleased() {
+  blockPosistion();
+}
+
+function createScreen() {
+  // fill('gray');
+  // rect(width/2 - screenWidth/2, 0, screenWidth, screenHeight);
+  let screen = {
+    x: width/2 - screenWidth/2,
+    y: 0,
+    w: screenWidth,
+    h: height,
+  };
+  return screen;
+}
+
+function blockPosistion() {
+  let xPos = aBlock.x1 - screen.x;
+  if (xPos % blockSize !== 0) {
+    xPos /= blockSize;
+    xPos = round(xPos);
+    xPos *= blockSize;
+    xPos += screen.x;
+    
+    aBlock.x1 = xPos;
+  }
+
 }
