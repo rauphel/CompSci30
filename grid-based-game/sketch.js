59848c7e09868a1ab188256138db1c82d10df3e8
rauphel <rauphelcyreuskenept@gmail.com>
2025-10-30 09:54:34 -0600

    following Coding train terrain generation turtorial

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -5,12 +5,29 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let rows, cols;
+const SCALE = 50;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth, windowHeight);
+  rows = height / SCALE;
+  cols = width / SCALE;
+
 }
 
 function draw() {
   background(220);
-  
+  terrainGeneration();
+}
+
+
+function terrainGeneration() {
+  for (let y = 0; y < rows; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (let x = 0; x < cols; x++) {
+      vertex(x * SCALE, y * SCALE);
+      vertex(x * SCALE, (y + 1) * SCALE);
+    }
+    endShape();
+  }
 }
