859729e035175bb96f5a189d251395c6545f1412
rauphel <rauphelcyreuskenept@gmail.com>
2025-11-12 18:33:06 -0600

    added other movements

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -5,24 +5,20 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// initializes grid for triangles and its scale
+// initializes grid for triangles width and length and its SUBDIVISIONS
 let rows, cols;
-const SCALE = 20;
+const SUBDIVISIONS = 20;
 const TERRAIN_X = 1000;
 const TERRAIN_Y = 1000;
 
 // 2d array keeping the z values of each grid
 let terrainHeight;
-let seed;
+let seed;           // noise seed saved as a global variable to be able to save it
 
 // player camera and coordinates
 let playerCam;
-let camX;
-let camY;
-let CamZ;
 
-
-const SPEED = 20;
+// saved camera angle restrictions as constant outside of class; not too sure how to make constants in classes 
 const MAX_PITCH = 180;
 const MIN_PITCH = 0;
 
@@ -30,33 +26,32 @@ function setup() {
   // sets up 3d renderer
   createCanvas(windowWidth, windowHeight, WEBGL);
 
-  // sets terrain / grid size
-  rows = TERRAIN_Y / SCALE;
-  cols = TERRAIN_X / SCALE;
-  seed = random();
+  // sets terrain / grid size / generates a random seed and makes 2d arrays with the heights
+  rows = TERRAIN_Y / SUBDIVISIONS;
+  cols = TERRAIN_X / SUBDIVISIONS;
+  seed = random(1, 100);
   terrainHeight = generateHeight(cols, rows, seed);
 
-  // creates and sets player Camera
-  playerCam = new Mover(0, 0, 0, 0.1);
+  // creates and sets player Camera 
+  playerCam = new Mover(0, -50, 0);
 }
 
 function draw() {
   background(220);
 
-  // orbitControl();
-
   // showPlane();
   showTerrain();
-  
+   
+  // updates camera which holds movement and direction
   playerCam.update();
-  
 }
 
-function generateHeight(cols, rows, seed) {
+function generateHeight(cols, rows, seed) { // generates a 2d grid using perlin noise 
+  // offset is the distance between each point 
   let newGrid = [];
   let yOffset = 0;
 
-  noiseSeed(seed);
+  noiseSeed(seed);  // sets the seed
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     let xOffset = 0;
@@ -70,29 +65,29 @@ function generateHeight(cols, rows, seed) {
 }
 
 function showTerrain() {
-  push();
+  push(); // isolates translations
   translate(-TERRAIN_X/2, 0, -TERRAIN_Y/2);
   rotateX(PI/2);
 
-  for (let y = 0; y < rows - 1; y++) {
+  for (let y = 0; y < rows - 1; y++) { // generates a triangles strip to display terrain using generated heights in 2d array
     beginShape(TRIANGLE_STRIP);
     for (let x = 0; x < cols; x++) {
-      vertex(x * SCALE, y * SCALE, terrainHeight[y][x]);
-      vertex(x * SCALE, (y + 1) * SCALE, terrainHeight[y + 1][x]);
+      vertex(x * SUBDIVISIONS, y * SUBDIVISIONS, terrainHeight[y][x]);
+      vertex(x * SUBDIVISIONS, (y + 1) * SUBDIVISIONS, terrainHeight[y + 1][x]);
     }
     endShape();
   }
   pop();
 }
 
-function keyPressed() {
+function keyPressed() { // seed randomizer and gets new heights
   if (key === "r") {
-    seed = random();
+    seed = random(1, 100);
     terrainHeight = generateHeight(cols, rows, seed);
   }
 }
 
-function showPlane() {
+function showPlane() { // plane for tests 
   push();
   rotateX(PI/2);
   plane(TERRAIN_X, TERRAIN_Y);
@@ -100,18 +95,19 @@ function showPlane() {
 }
 
 class Mover {
-  constructor(x, y, z, sense) {
-    this.x = x;
+  constructor(x, y, z) { 
+    this.x = x; // world coordinates
     this.y = y;
     this.z = z;
-    this.cam = createCamera();
+    this.cam = createCamera(); // creates cam and sets its positions
     this.cam.setPosition(this.x, this.y, this.z);
 
-    this.rX = 0;
-    this.rY = 0;
-    this.camVector;
+    this.rY = 0;  // rotation based on the y-axis
+    this.rX = 90; // rotation based on the x-axis
+    this.camVector; //vector for where it's looking
 
-    this.sensitivity = sense;
+    this.sensitivity = 0.1; //cam sensitivity and speed
+    this.speed = 5;
   }
 
   update() {
@@ -125,15 +121,15 @@ class Mover {
   }
 
   look() { //change to constants
-    this.rX -= movedX * this.sensitivity;
-    this.rY += movedY * this.sensitivity;
+    this.rY -= movedX * this.sensitivity;
+    this.rX += movedY * this.sensitivity;
 
-    this.rY = constrain(this.rY, MIN_PITCH, MAX_PITCH); // figure out
-    this.rX = this.rX % 360;
+    this.rX = constrain(this.rX, MIN_PITCH, MAX_PITCH); // figure out
+    this.rY = this.rY % 360;
   }
 
   pointCam() {
-    this.camVector = p5.Vector.fromAngles(radians(this.rY), radians(this.rX)); // figure out
+    this.camVector = p5.Vector.fromAngles(radians(this.rX), radians(this.rY)); // figure out
     // this.cam.setPosition(this.x, this.y, this.z);
     this.cam.lookAt(this.camVector.x + this.cam.eyeX, this.camVector.y + this.cam.eyeY, this.camVector.z + this.cam.eyeZ);
 
@@ -141,17 +137,27 @@ class Mover {
     // console.log(this.camVector.toString());
   }
   move() {
-    if (keyIsDown(87)) {
-      // this.heading = p5.Vector.normalize(this.camVector);
-      // translate(0, 0, -1);
-      this.cam.move(0, 0, -1);
-      // console.log(this.cam.eyeX);
-      
-      
-      // this.x = this.cam.eyeX;
-      // this.y = this.cam.eyeY;
-      // this.z = this.cam.eyeZ;
+    if (keyIsDown(87)) { // w
+      this.cam.move(0, 0, -1 * this.speed);
     }
+    if (keyIsDown(83)) { // s
+      this.cam.move(0, 0, 1 * this.speed);
+    }
+    if (keyIsDown(65)) { // a
+      this.cam.move(-1 * this.speed, 0, 0);
+    }
+    if (keyIsDown(68)) { // d
+      this.cam.move(1 * this.speed, 0, 0);
+    }
+    if (keyIsDown(16)) { // shift
+      this.cam.move(0, 1 * this.speed, 0);
+    }
+    if (keyIsDown(32)) { // space
+      this.cam.move(0, -1 * this.speed, 0);
+    }
+    this.x = this.cam.eyeX;
+    this.y = this.cam.eyeY;
+    this.z = this.cam.eyeZ;
   }
 }
 
